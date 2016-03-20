2fba471fc30076a55af590ea3c8bc10017fce373
Jarno Elovirta <jarno@elovirta.com>
2016-03-20 15:28:13 +0200

    Move CI scripts to CI subdirectory

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,4 +8,4 @@ export SITE_DIR=$PWD/dita-ot.github.io
 cd $SITE_DIR
 git commit -a -m "Deploy $TRAVIS_COMMIT_RANGE for dev"
 # push
-# TODO
+# TODO https://docs.travis-ci.com/user/deployment/custom/#Git

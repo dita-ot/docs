2ffac18e667fcf81f4171dfef4c00c4190849640
Jarno Elovirta <jarno@elovirta.com>
2016-03-20 17:55:43 +0200

    Add site deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,8 +4,14 @@
 
 export SITE_DIR=$PWD/dita-ot.github.io
 
+git config user.email "ditaotbot@gmail.com"
+git config user.name "DITA-OT Bot"
+
 # commit site
 cd $SITE_DIR
 git commit -a -m "Deploy $TRAVIS_COMMIT_RANGE for dev"
 # push
-# TODO https://docs.travis-ci.com/user/deployment/custom/#Git
+eval "$(ssh-agent -s)"
+chmod 600 .travis/ditaotbot_rsa.pub
+ssh-add .travis/ditaotbot_rsa.pub
+git push origin develop

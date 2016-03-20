710933b161de499da815f3a7488381f3a888547b
Jarno Elovirta <jarno@elovirta.com>
2016-03-20 17:52:00 +0200

    Use Gradle wrapper in CI build

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -6,4 +6,4 @@ export DITA_HOME=$PWD/dita-ot-$DITA_OT_VERSION
 export SITE_DIR=$PWD/dita-ot.github.io
 
 # run site generation into 
-gradle -b site.gradle -Ddita.home=$DITA_HOME -Doutput.dir=$SITE_DIR/dev --info --stacktrace --no-daemon
+./gradlew -b site.gradle -Ddita.home=$DITA_HOME -Doutput.dir=$SITE_DIR/dev --info --stacktrace --no-daemon

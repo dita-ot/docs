94ae2c5a46252efdac3e945136953ab3995f7476
Jarno Elovirta <jarno@elovirta.com>
2016-03-20 20:16:22 +0200

    Fix CI deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,16 +2,16 @@
 
 # CI deployment script
 
+export SSH_DIR=$PWD/.travis
 export SITE_DIR=$PWD/dita-ot.github.io
-
-git config user.email "ditaotbot@gmail.com"
-git config user.name "DITA-OT Bot"
 
 # commit site
 cd $SITE_DIR
-git commit -a -m "Deploy $TRAVIS_COMMIT_RANGE for dev"
+git config user.email "ditaotbot@gmail.com"
+git config user.name "DITA-OT Bot"
+git commit -a -m "Deploy dita-ot/docs@$TRAVIS_COMMIT to dev"
 # push
 eval "$(ssh-agent -s)"
-chmod 600 .travis/ditaotbot_rsa.pub
-ssh-add .travis/ditaotbot_rsa.pub
+chmod 600 $SSH_DIR/ditaotbot_rsa.pub
+ssh-add $SSH_DIR/ditaotbot_rsa.pub
 git push origin develop

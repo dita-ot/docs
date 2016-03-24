bb2249d13c7f2a398a30cfe23f75f5a2e1e927cf
Jarno Elovirta <jarno@elovirta.com>
2016-03-24 18:30:05 +0200

    Fix CI deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   # commit site
   git config user.email "ditaotbot@gmail.com"
   git config user.name "DITA-OT Bot"
-  git commit -a -m "Deploy ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT:0:7} to 'dev' docs"
+  git commit -a -m "Deploy dita-ot/docs@${TRAVIS_COMMIT:0:7} to 'dev' docs"
   # push
   git remote set-url origin git@github.com:dita-ot/dita-ot.github.io.git
   git push -v origin develop

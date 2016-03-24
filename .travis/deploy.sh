c7cc91116f59c3e816601b0471a1b0d9bbdbfcff
Jarno Elovirta <jarno@elovirta.com>
2016-03-24 16:46:31 +0200

    Run conversion for all builds, but only deploy on develop

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,20 +2,22 @@
 
 # CI deployment script
 
-export SSH_DIR=$PWD/.travis
-export SITE_DIR=$PWD/dita-ot.github.io
+if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop"]; then
+  export SSH_DIR=$PWD/.travis
+  export SITE_DIR=$PWD/dita-ot.github.io
 
-# install SSH key
-eval "$(ssh-agent -s)"
-chmod 600 $PWD/.travis/ditaotbot_rsa
-ssh-add $PWD/.travis/ditaotbot_rsa
+  # install SSH key
+  eval "$(ssh-agent -s)"
+  chmod 600 $PWD/.travis/ditaotbot_rsa
+  ssh-add $PWD/.travis/ditaotbot_rsa
 
-cd $SITE_DIR
+  cd $SITE_DIR
 
-# commit site
-git config user.email "ditaotbot@gmail.com"
-git config user.name "DITA-OT Bot"
-git commit -a -m "Deploy dita-ot/docs@${TRAVIS_COMMIT:0:7} to 'dev' docs"
-# push
-git remote set-url origin git@github.com:dita-ot/dita-ot.github.io.git
-git push -v origin develop
+  # commit site
+  git config user.email "ditaotbot@gmail.com"
+  git config user.name "DITA-OT Bot"
+  git commit -a -m "Deploy ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT:0:7} to 'dev' docs"
+  # push
+  git remote set-url origin git@github.com:dita-ot/dita-ot.github.io.git
+  git push -v origin develop
+fi

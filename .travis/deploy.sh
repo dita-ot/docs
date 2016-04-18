de2acbcb63a59f92c80680c9acac37752262d217
Roger Sheen <infotexture@gmail.com>
2016-04-18 14:50:16 +0200

    Add/remove output files generated for CI builds

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# CI deployment script
+# CI deployment script (runs after publish script)
 
 if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   export SSH_DIR=$PWD/.travis
@@ -13,11 +13,14 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
 
   cd $SITE_DIR
 
-  # clean
+  # Reset files where only commit metadata has changed
   ruby ../.travis/clean.rb
   # commit site
   git config user.email "ditaotbot@gmail.com"
   git config user.name "DITA-OT Bot"
+  # Add (stage) all untracked & modified files and remove all missing files
+  git add --all $SITE_DIR/dev
+  # Commit generated site output
   git commit -a -m "Deploy dita-ot/docs@${TRAVIS_COMMIT:0:7} to 'dev' docs"
   # push
   git remote set-url origin git@github.com:dita-ot/dita-ot.github.io.git

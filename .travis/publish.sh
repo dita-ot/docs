7021c628864cb1b1edc9d39497a5924011ecfd3f
Roger Sheen <infotexture@gmail.com>
2016-04-18 16:14:17 +0200

    Remove & re-create output directory for CI builds

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -7,9 +7,9 @@ export DITA_OT_DEV=$(find $PWD -name 'dita-ot-*+*' -type d | head -1)
 export SITE_DIR=$PWD/dita-ot.github.io
 
 #if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
-  # Clean target output directory before generating new output
-  rm -rf "$SITE_DIR/dev/*"
-  # Generate site output in /dev folder
+  # Remove target output directory before generating new output
+  rm -rf "$SITE_DIR/dev"
+  # Re-create /dev folder & generate site output there
   ./gradlew -b site.gradle -Ddita.home=$DITA_HOME -Doutput.dir=$SITE_DIR/dev -PditaHomeSrc=$DITA_OT_DEV --info --stacktrace --no-daemon
 #else
 #  ./gradlew -Ddita.home=$DITA_HOME html --info --stacktrace --no-daemon

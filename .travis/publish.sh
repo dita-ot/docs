b495638d2129a69cff5c466f66f36fcb435a40cb
Roger Sheen <roger@infotexture.net>
2017-10-20 13:29:23 +0200

    Publish w/ latest DITA-OT from 'develop' branch
    Signed-off-by: Roger Sheen <roger@infotexture.net>

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -2,15 +2,20 @@
 
 # CI publish script
 
+# Stable DITA-OT version
 export DITA_HOME=$PWD/dita-ot-$DITA_OT_VERSION
+
+# Latest DITA-OT version from 'develop' branch
 export DITA_OT_DEV=$(find $PWD -name 'dita-ot-*+*' -type d | head -1)
+
+# Site directory for GitHub Pages
 export SITE_DIR=$PWD/dita-ot.github.io
 
 #if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   # Remove target output directory before generating new output
   rm -rf "$SITE_DIR/dev"
   # Re-create /dev folder & generate site output there
-  ./gradlew -b site.gradle -Ddita.home=$DITA_HOME -Doutput.dir=$SITE_DIR/dev -PditaHomeSrc=$DITA_OT_DEV --info --stacktrace --no-daemon
+  ./gradlew -b site.gradle -Ddita.home=$DITA_OT_DEV -Doutput.dir=$SITE_DIR/dev -PditaHomeSrc=$DITA_OT_DEV --info --stacktrace --no-daemon
 #else
 #  ./gradlew -Ddita.home=$DITA_HOME html --info --stacktrace --no-daemon
 #fi

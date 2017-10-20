8e658d2960577657cf6c1f568c3b9623dc6e8a5f
Roger Sheen <roger@infotexture.net>
2017-10-20 14:07:06 +0200

    Install site plugin into latest 'develop' DITA-OT
    Signed-off-by: Roger Sheen <roger@infotexture.net>

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,19 +8,20 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   openssl aes-256-cbc -K $encrypted_324699cc92df_key -iv $encrypted_324699cc92df_iv -in .travis/ditaotbot_rsa.enc -out .travis/ditaotbot_rsa -d
 fi
 
-# download stable dita-ot
+# Download stable DITA-OT version
 curl -L https://github.com/dita-ot/dita-ot/releases/download/$DITA_OT_VERSION/dita-ot-$DITA_OT_VERSION.zip -o dita-ot-$DITA_OT_VERSION.zip
 unzip dita-ot-$DITA_OT_VERSION.zip
 export DITA_HOME=$PWD/dita-ot-$DITA_OT_VERSION
 
-# download develop dita-ot
+# Download latest DITA-OT version from 'develop' branch
 curl -L https://s3-eu-west-1.amazonaws.com/dita-ot/dita-ot-develop.zip -o dita-ot-develop.zip
 unzip dita-ot-develop.zip
+export DITA_OT_DEV=$(find $PWD -name 'dita-ot-*+*' -type d | head -1)
 
 #if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   # install site plugin
   curl -L https://github.com/dita-ot/org.dita-ot.html/archive/master.zip -o org.dita-ot.html.zip
-  $DITA_HOME/bin/dita -install org.dita-ot.html.zip -v
+  $DITA_OT_DEV/bin/dita -install org.dita-ot.html.zip -v
 
   # clone site
   git clone -b master https://github.com/dita-ot/dita-ot.github.io.git

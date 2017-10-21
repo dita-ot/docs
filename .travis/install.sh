c1b4da0084cbedede8a302fed116bb244eae6860
Roger Sheen <roger@infotexture.net>
2017-10-21 18:38:54 +0200

    Install additional plug-in for site builds
    Signed-off-by: Roger Sheen <roger@infotexture.net>

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,20 +8,22 @@ if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   openssl aes-256-cbc -K $encrypted_324699cc92df_key -iv $encrypted_324699cc92df_iv -in .travis/ditaotbot_rsa.enc -out .travis/ditaotbot_rsa -d
 fi
 
-# download stable dita-ot
+# Download stable DITA-OT version (`publish.sh` uses this to run the conversion)
 curl -L https://github.com/dita-ot/dita-ot/releases/download/$DITA_OT_VERSION/dita-ot-$DITA_OT_VERSION.zip -o dita-ot-$DITA_OT_VERSION.zip
 unzip dita-ot-$DITA_OT_VERSION.zip
 export DITA_HOME=$PWD/dita-ot-$DITA_OT_VERSION
 
-# download develop dita-ot
+# Download latest 'develop' DITA-OT version (`publish.sh` reads plugin configuration for generated topics from here)
 curl -L https://s3-eu-west-1.amazonaws.com/dita-ot/dita-ot-develop.zip -o dita-ot-develop.zip
 unzip dita-ot-develop.zip
 
 #if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
-  # install site plugin
+  # Install site plugin
   curl -L https://github.com/dita-ot/org.dita-ot.html/archive/master.zip -o org.dita-ot.html.zip
   $DITA_HOME/bin/dita -install org.dita-ot.html.zip -v
+  # Install LwDITA plugin
+  $DITA_HOME/bin/dita --install https://github.com/jelovirt/org.lwdita/releases/download/2.0/org.lwdita-2.0.zip
 
-  # clone site
+  # Clone site
   git clone -b master https://github.com/dita-ot/dita-ot.github.io.git
 #fi

53997ffb5642a546dce5dd86498f301dd0cc6400
Jarno Elovirta <jarno@elovirta.com>
2018-01-04 16:13:26 +0200

    Retain generated files and redirect files on CI site build
    Signed-off-by: Jarno Elovirta <jarno@elovirta.com>

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -8,7 +8,17 @@ export SITE_DIR=$PWD/dita-ot.github.io
 
 #if [ "$TRAVIS_PULL_REQUEST" = "false" -a "$TRAVIS_BRANCH" = "develop" ]; then
   # Remove target output directory before generating new output
-  rm -rf "$SITE_DIR/dev"
+  for i in `find "$SITE_DIR/dev" -type f`; do
+    if grep -q 'redirect_to:' "$i"; then
+      echo "Skip redirect file $i"
+    elif grep -q 'Generated from DITA source' "$i"; then
+      echo "Skip generated file $i"
+    elif grep -q 'generated: true' "$i"; then
+      echo "Skip generated file $i"
+    else
+      rm "$i"
+    fi
+  done
   # Re-create /dev folder & generate site output there
   ./gradlew site -PditaHome=$DITA_HOME -PoutputDir=$SITE_DIR/dev -PditaHomeSrc=$DITA_OT_DEV --info --stacktrace --no-daemon
 #else
